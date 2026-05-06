98b49ade43b88e2998ae312538e4b8747bdf38d6
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-05-06 18:16:14 +0000

    Automated update

--- src/Soenneker.OpenAI.OpenApiClient/Models/CostsResult.cs
+++ src/Soenneker.OpenAI.OpenApiClient/Models/CostsResult.cs
@@ -49,6 +49,14 @@ namespace Soenneker.OpenAI.OpenApiClient.Models
 #else
         public global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult.CostsResult_project_id ProjectId { get; set; }
 #endif
+        /// <summary>The quantity property</summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+        public global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult.CostsResult_quantity? Quantity { get; set; }
+#nullable restore
+#else
+        public global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult.CostsResult_quantity Quantity { get; set; }
+#endif
         /// <summary>
         /// Instantiates a new <see cref="global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult"/> and sets the default values.
         /// </summary>
@@ -79,6 +87,7 @@ namespace Soenneker.OpenAI.OpenApiClient.Models
                 { "line_item", n => { LineItem = n.GetObjectValue<global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult.CostsResult_line_item>(global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult.CostsResult_line_item.CreateFromDiscriminatorValue); } },
                 { "object", n => { Object = n.GetEnumValue<global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult_object>(); } },
                 { "project_id", n => { ProjectId = n.GetObjectValue<global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult.CostsResult_project_id>(global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult.CostsResult_project_id.CreateFromDiscriminatorValue); } },
+                { "quantity", n => { Quantity = n.GetObjectValue<global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult.CostsResult_quantity>(global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult.CostsResult_quantity.CreateFromDiscriminatorValue); } },
             };
         }
         /// <summary>
@@ -93,6 +102,7 @@ namespace Soenneker.OpenAI.OpenApiClient.Models
             writer.WriteObjectValue<global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult.CostsResult_line_item>("line_item", LineItem);
             writer.WriteEnumValue<global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult_object>("object", Object);
             writer.WriteObjectValue<global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult.CostsResult_project_id>("project_id", ProjectId);
+            writer.WriteObjectValue<global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult.CostsResult_quantity>("quantity", Quantity);
             writer.WriteAdditionalData(AdditionalData);
         }
         /// <summary>
@@ -296,6 +306,68 @@ namespace Soenneker.OpenAI.OpenApiClient.Models
                 }
                 else {
                     writer.WriteObjectValue<global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult_project_idMember1>(null, CostsResultProjectIdMember1);
+                }
+            }
+        }
+        /// <summary>
+        /// Composed type wrapper for classes <see cref="double"/>, <see cref="global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult_quantityMember1"/>
+        /// </summary>
+        [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
+        public partial class CostsResult_quantity : IComposedTypeWrapper, IParsable
+        {
+            /// <summary>Composed type representation for type <see cref="global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult_quantityMember1"/></summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+            public global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult_quantityMember1? CostsResultQuantityMember1 { get; set; }
+#nullable restore
+#else
+            public global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult_quantityMember1 CostsResultQuantityMember1 { get; set; }
+#endif
+            /// <summary>Composed type representation for type <see cref="double"/></summary>
+            public double? Double { get; set; }
+            /// <summary>
+            /// Creates a new instance of the appropriate class based on discriminator value
+            /// </summary>
+            /// <returns>A <see cref="global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult.CostsResult_quantity"/></returns>
+            /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
+            public static global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult.CostsResult_quantity CreateFromDiscriminatorValue(IParseNode parseNode)
+            {
+                if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
+                var result = new global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult.CostsResult_quantity();
+                if(parseNode.GetDoubleValue() is double doubleValue)
+                {
+                    result.Double = doubleValue;
+                }
+                else {
+                    result.CostsResultQuantityMember1 = new global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult_quantityMember1();
+                }
+                return result;
+            }
+            /// <summary>
+            /// The deserialization information for the current model
+            /// </summary>
+            /// <returns>A IDictionary&lt;string, Action&lt;IParseNode&gt;&gt;</returns>
+            public virtual IDictionary<string, Action<IParseNode>> GetFieldDeserializers()
+            {
+                if(CostsResultQuantityMember1 != null)
+                {
+                    return ParseNodeHelper.MergeDeserializersForIntersectionWrapper(CostsResultQuantityMember1);
+                }
+                return new Dictionary<string, Action<IParseNode>>();
+            }
+            /// <summary>
+            /// Serializes information the current object
+            /// </summary>
+            /// <param name="writer">Serialization writer to use to serialize this model</param>
+            public virtual void Serialize(ISerializationWriter writer)
+            {
+                if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
+                if(Double != null)
+                {
+                    writer.WriteDoubleValue(null, Double);
+                }
+                else {
+                    writer.WriteObjectValue<global::Soenneker.OpenAI.OpenApiClient.Models.CostsResult_quantityMember1>(null, CostsResultQuantityMember1);
                 }
             }
         }
