b554b4457da620d42971b42054b0a77ad87bdcd7
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-09-01 19:43:32 +0000

    Automated update

--- src/Soenneker.OpenAI.OpenApiClient/Models/BetaInputItem.cs
+++ src/Soenneker.OpenAI.OpenApiClient/Models/BetaInputItem.cs
@@ -8,7 +8,7 @@ using System;
 namespace Soenneker.OpenAI.OpenApiClient.Models
 {
     /// <summary>
-    /// A list of one or many input items to the model, containing different content types.
+    /// A list of one or many input items to the model, containingdifferent content types.
     /// </summary>
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     public partial class BetaInputItem : IAdditionalDataHolder, IParsable
